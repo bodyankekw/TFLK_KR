d53aa4c3419f5fee7c20797352ef519faf94dd5c
bodyanlul <55473834+bodyanlul@users.noreply.github.com>
2022-05-16 22:52:56 +0700

    Add files via upload

---
+++ Scanner.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tf9ik
+{
+    public class Scanner
+    {
+        enum Lexemes
+        {
+            access = 0,
+            name = 1,
+            strAs = 2,
+            type = 3,
+            equal = 4,
+            const_value = 5,
+            comma = 6,
+            enter = 7,
+            error = -1
+        }
+
+        public List<ScanResult> Scan(string line)
+        {
+            int numberString = 0, stringPosition = 0;
+            List<ScanResult> scanResults = new List<ScanResult>();
+            if (line.Length == 0)
+            {
+                return scanResults;
+            }
+
+            string currentValue = string.Empty;
+            char currentLetter = line[0];
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                ScanResult result = new ScanResult();
+                currentValue = string.Empty;
+                currentLetter = line[i];
+
+                if (line[i].Equals(' ') || line[i] == '\n')
+                {
+                    if (line[i] == '\n')
+                    {
+                        result.ElementCode = Convert.ToInt32(Lexemes.enter);
+                        result.Value = "переход строки";
+                        result.Position = i;
+                        result.NumberString = numberString;
+                        result.PositionString = stringPosition;
+                        scanResults.Add(result);
+                        stringPosition = i;
+                        numberString++;
+                    }
+                    continue;
+                }
+
+                if (char.IsLetter(line[i]))
+                {
+                    currentValue += line[i];
+                    result.Position = i++;
+                    result.NumberString = numberString;
+                    result.PositionString = stringPosition;
+
+                    while (i < line.Length)
+                    {
+                        if (char.IsDigit(line[i]) || char.IsLetter(line[i]) || line[i].Equals('_'))
+                        {
+                            currentValue += line[i];
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    switch (currentValue)
+                    {
+                        case "Dim":
+                        case "Static":
+                        case "Public":
+                        case "Private":
+                            result.ElementCode = Convert.ToInt32(Lexemes.access);
+                            break;
+                        case "Double":
+                        case "Integer":
+                            result.ElementCode = Convert.ToInt32(Lexemes.type);
+                            break;
+                        case "As":
+                            result.ElementCode = Convert.ToInt32(Lexemes.strAs);
+                            break;
+                        default:
+                            result.ElementCode = Convert.ToInt32(Lexemes.name);
+                            break;
+                    }
+                    result.Value = currentValue;
+                    scanResults.Add(result);
+                    i--;
+                    continue;
+                }
+
+                if (char.IsDigit(line[i]))
+                {
+                    result.Position = i;
+                    result.NumberString = numberString;
+                    result.PositionString = stringPosition;
+                    result.ElementCode = Convert.ToInt32(Lexemes.const_value);
+
+                    currentValue += line[i++];
+                    bool onePoint = false;
+
+                    while (i < line.Length)
+                    {
+                        if (line[i].Equals('.'))
+                        {
+                            if (!onePoint)
+                            {
+                                onePoint = true;
+                                currentValue += ".";
+                                i++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        if (char.IsDigit(line[i]))
+                        {
+                            currentValue += line[i];
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    result.Value = currentValue;
+                    scanResults.Add(result);
+                    i--;
+                    continue;
+                }
+
+                if (line[i] == '=')
+                {
+                    result.Position = i;
+                    result.NumberString = numberString;
+                    result.PositionString = stringPosition;
+                    result.ElementCode = Convert.ToInt32(Lexemes.equal);
+                    result.Value = "=";
+                    scanResults.Add(result);
+                    continue;
+                }
+
+                if (line[i] == ',')
+                {
+                    result.Position = i;
+                    result.NumberString = numberString;
+                    result.PositionString = stringPosition;
+                    result.ElementCode = Convert.ToInt32(Lexemes.comma);
+                    result.Value = ",";
+                    scanResults.Add(result);
+                    continue;
+                }
+
+                result.Position = i;
+                result.NumberString = numberString;
+                result.PositionString = stringPosition;
+                result.ElementCode = Convert.ToInt32(Lexemes.error);
+                result.Value = "error";
+                scanResults.Add(result);
+            }
+            return scanResults;
+        }
+        public string expected(int error_code)
+        {
+            switch (error_code)
+            {
+                case 0:
+                    return "идентификатор доступа(Dim, Static, Public, Private)";
+                case -1:
+                    return "имя переменной";
+                case -2:
+                    return "имя переменной";
+                case -3:
+                    return "As";
+                case -4:
+                    return "тип переменной(Integer, Double)";
+                case -5:
+                    return "=";
+                case -6:
+                    return "константа";
+                case -7:
+                    return "\',\' или идентификатор доступа(Dim, Static, Public, Private) или конец строки";
+                case -8:
+                    return "= или переход строки";
+                default:
+                    return "не опредлено";
+            }
+        }
+
+        public string Continue(int error_code)
+        {
+            switch (error_code)
+            {
+                case 0:
+                    return "<Access id> <Name>  As <Type>";
+                case -1:
+                    return "<Name> As <Type>";
+                case -2:
+                    return "<Name> As <Type>";
+                case -3:
+                    return "As";
+                case -4:
+                    return "<Type> = <Value>";
+                case -5:
+                    return "= <Value>, все константы при объявлении должны быть определены ";
+                case -6:
+                    return "<Value>";
+                case -7:
+                    return "";
+                case -8:
+                    return "= или переход строки";
+                default:
+                    return "не опредлено";
+            }
+        }
+    }
+}
+
---
+++ SyntaxAnalyzer.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tf9ik
+{
+    struct findedError
+    {
+        public int position;
+        public int stringNumber;
+        public int positionString;
+        public int code;
+        public string value;
+        public int i;
+        public findedError(int code, int position, int stringNumber, int positionString, string value, int i)
+        {
+            this.position = position;
+            this.stringNumber = stringNumber;
+            this.positionString = positionString;
+            this.code = code;
+            this.value = value;
+            this.i = i;
+        }
+    }
+
+    class SyntaxAnalyzer
+    {
+        private List<findedError> erroors;
+
+        public List<findedError> Errors(List<ScanResult> scanResults)
+        {
+            erroors = new List<findedError>();
+            for (int i = 0; i < scanResults.Count;)
+            {
+                i = Analysis(scanResults, i) + 1;
+                for (; i < scanResults.Count; i++)
+                {
+                    if (scanResults[i].ElementCode == 0) break;
+                }
+            }
+            return erroors;
+        }
+
+
+        private int Analysis(List<ScanResult> scanResults, int i)
+        {
+            int state = 0;
+            bool warning = true;
+            if (state == 0 && scanResults[i].ElementCode != 0)
+            {
+                erroors.Add(new findedError(0, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                state = 2;
+               
+            }
+            i++;
+            for (; i < scanResults.Count; i++)
+            {
+                switch (state)
+                {
+                    case 0://name
+                        if (scanResults[i].ElementCode == 1) //name 
+                            state = 2;
+                        else
+                        {
+                            state = 2;
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                        }
+                        break;
+                    case 1://name 
+                        state = 2;
+                        if (scanResults[i].ElementCode != 1)
+                        {
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 2) i--;
+                        }
+                        break;
+                    case 2://As 
+                        state = 3;
+                        if (scanResults[i].ElementCode != 2)
+                        {
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 3) i--;
+                        }
+                        break;
+                    case 3://type 
+                        state = 4;
+                        if (scanResults[i].ElementCode != 3)
+                        {
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 4) i--;
+                        }
+                        break;
+                    case 4://'=' or ',' or '/n' 
+                        if (scanResults[i].ElementCode == 4)
+                        {
+                            state = 5;
+                        }
+                        else if (scanResults[i].ElementCode == 7)
+                        {
+                           state = 8;
+                        }
+                        else if (scanResults[i].ElementCode == 6)
+                        {
+                           state = 7;
+                        }
+                        else
+                        {
+                            state = 8;
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 0) i--;
+                        }
+                        break;
+                    case 5://const 
+                        state = 6;
+                        if (scanResults[i].ElementCode != 5)
+                        {
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 6 || scanResults[i].ElementCode == 7) i--;
+                        }
+                        break;
+                    case 6://',' or '/n' 
+                        if (scanResults[i].ElementCode == 6)
+                        {
+                            state = 7;
+                        }
+                        else if (scanResults[i].ElementCode == 7)
+                        {
+                            
+                            state = 8;
+                        }
+                        else
+                        {
+                            state = 7;
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 1 || scanResults[i].ElementCode == 5) i--;
+                        }
+                        break;
+                    case 7://name 
+                        if (scanResults[i].ElementCode == 1)
+                        {
+                            state = 2;
+                        }
+                        else if (scanResults[i].ElementCode == 5)
+                        {
+                            double num = System.Convert.ToDouble(scanResults[i - 2].Value);
+                            if (num % 1 > 0 && warning == false)
+                                state = -8;
+                            else
+                            {
+                                state = 6;
+                                erroors.Add(new findedError(1, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, "вместо `.` встречено `,`", i));
+                            }
+                        }
+                        else
+                        {
+                            state = 8;
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 0 || scanResults[i].ElementCode == 7) i--;
+                        }
+                        break;
+                    case 8://end 
+                        if (scanResults[i].ElementCode == 0)
+                        {
+                            state = 0;
+                        }
+                        else if (scanResults[i].ElementCode == 7)
+                        {
+                            state = 8;
+                        }
+                        else
+                        {
+                            state = 6;
+                            erroors.Add(new findedError(-state, scanResults[i].Position, scanResults[i].NumberString, scanResults[i].PositionString, scanResults[i].Value, i));
+                            if (scanResults[i].ElementCode == 6 || scanResults[i].ElementCode == 7) i--;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (state == 4 || state == 6)
+            {
+                return i;
+            }
+            else
+            {
+                erroors.Add(new findedError(-state - 1, scanResults[i - 1].Position, scanResults[i - 1].NumberString, scanResults[i - 1].PositionString, "пусто", i - 1));
+                return i - 1;
+            }
+        }
+    }
+}
